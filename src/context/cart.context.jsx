9fe6e4f3770bfd0faae6c5e18642ceee426140fc
Efunnuga Bamidele <bamideleefunnuga9@gmail.com>
2022-11-04 12:09:54 +0100

    Feet: Implemented the items add to cart function

--- src/context/cart.context.jsx
+++ src/context/cart.context.jsx
@@ -1,17 +1,44 @@
 import { createContext, useState } from "react";
 
 
+export const addCartItem = (cartItems, productToAdd) => {
+   // find if cartItems contains productToAdd
+   const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === productToAdd.id
+    );
+    
+   //if found increment quantity
+   if(existingCartItem){
+    return cartItems.map((cartItem) => (
+        cartItem.id === productToAdd.id 
+        ? { ...cartItem, quantity: cartItem.quantity + 1}
+        : cartItem
+        ))
+   }else{
+        return [ ...cartItems, { ...productToAdd, quantity: 1}];
+   }
+
+   
+}
+
 export const CartContext = createContext({
     isCartOpen: false,
-    setIsCartOpen: () => false,
+    setIsCartOpen: () => {},
+    cartItems: [],
+    addItemToCart: () => {}
 
 });
 
 export const CartProvider = ({children}) => {
     const [isCartOpen, setIsCartOpen] = useState(false);
+    const [cartItems, setCartItems] = useState([]);
 
-    const value = {isCartOpen, setIsCartOpen};
-    console.log(value)
+    const addItemToCart = (productToAdd) => {
+        setCartItems(addCartItem(cartItems, productToAdd))
+    }
+
+    const value = {isCartOpen, setIsCartOpen, cartItems, addItemToCart};
+   
     return (
         <CartContext.Provider value={value}>
             {children}
