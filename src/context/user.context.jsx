8179e5269899df1d5fa6ba974d6fe43860dc62a5
Efunnuga Bamidele <bamideleefunnuga9@gmail.com>
2022-11-08 14:40:37 +0100

    feat: Implement useReducer for state management

--- src/context/user.context.jsx
+++ src/context/user.context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useEffect, useReducer } from "react";
 
 //firebase
 import { onAuthStateChangedListener, createUserDocumentFromAuth } from "../utils/firebase/firebase.utils";
@@ -9,8 +9,45 @@ export const UserContext = createContext({
     setCurrentUser: () => null,
 });
 
+
+//Implement Reducers
+export const USER_ACTION_TYPES = {
+    SET_CURRENT_USER: 'SET_CURRENT_USER'
+}
+
+const userReducer = (state, action) => {
+    console.log('dispatch')
+    console.log(action)
+
+    const { type, payload } = action;
+
+    switch(type){
+        case 'SET_CURRENT_USER':
+            return{
+                currentUser: payload
+            }
+        
+        default:
+            throw new Error(`Unhandled tyle ${type} in userReducer`)
+    }
+}
+
+const INITIAL_STATE = {
+    currentUser: null
+}
+
+//end of reducer implement
+
 export const UserProvider = ({ children }) => {
-    const [currentUser, setCurrentUser] = useState(null);
+    // const [currentUser, setCurrentUser] = useState(null);
+    const [ { currentUser }, dispatch ] = useReducer(userReducer, INITIAL_STATE)
+    console.log(currentUser)
+
+    const setCurrentUser = (user) => {
+        dispatch({type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user}); 
+    }
+
+
     const value = { currentUser, setCurrentUser };
     
 
@@ -31,3 +68,12 @@ export const UserProvider = ({ children }) => {
         </UserContext.Provider>
     )
 };
+
+/*
+const userReducer = (state, action) => {
+    return{
+        currentUser:''
+    }
+}
+
+*/
