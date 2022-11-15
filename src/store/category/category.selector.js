39da4d6aa028e4babd9fa1421d884a90e93109eb
Efunnuga Bamidele <bamideleefunnuga9@gmail.com>
2022-11-15 15:28:19 +0100

    feat: create custom logger middleware

--- src/store/category/category.selector.js
+++ src/store/category/category.selector.js
@@ -1,7 +1,8 @@
-export const selectCategoriesMap = (state) => 
-state.categories.categories.reduce((acc, category) => {
-    const { title, items } = category;
-    acc[title.toLowerCase()] = items;
-    return acc;
-}, {})
+export const selectCategoriesMap = (state) => {
+return state.categories.categories.reduce((acc, category) => {
+            const { title, items } = category;
+            acc[title.toLowerCase()] = items;
+            return acc;
+        }, {})
+}
 
