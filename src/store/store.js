10c57bb996231d5981ec80f03dd44f8be181a42e
Efunnuga Bamidele <bamideleefunnuga9@gmail.com>
2022-11-16 13:29:15 +0100

    feat: Install redux-persist and inplement persist function

--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,7 @@
 import { compose, legacy_createStore as createStore, applyMiddleware } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+
+import storage from 'redux-persist/lib/storage';
 
 // import logger from 'redux-logger';
 
@@ -21,10 +24,20 @@ const loggerMiddleware = (store) => (next) => (action) => {
     console.log('next state: ', store.getState());
 }
 
+const persistConfig ={
+    key: 'root',
+    storage: storage,
+    blaklist: ['user'],
+}
+
+const prtsistedReducer = persistReducer(persistConfig, rootReducer);
+
 const middleWares = [loggerMiddleware];
 
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+export const store = createStore(prtsistedReducer, undefined, composedEnhancers);
+
+export const persistor = persistStore(store);
 
 // export const store = configureStore({rootReducer, composedEnhancers});
